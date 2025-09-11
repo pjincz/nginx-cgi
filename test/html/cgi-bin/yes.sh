44f5bd9ff59eb423b37f6a1be68adf6ea16bae55
Chizhong Jin <pjincz@gmail.com>
2025-09-11 21:13:10 +0800

    fix unittest on OmniOS

--- test/html/cgi-bin/yes.sh
+++ test/html/cgi-bin/yes.sh
@@ -3,7 +3,7 @@
 echo "Content-Type: text/plain"
 echo
 
-w=$(echo "$QUERY_STRING" | sed 's/&/\n/' | sed -n "s/^w=//p")
-n=$(echo "$QUERY_STRING" | sed 's/&/\n/' | sed -n "s/^n=//p")
+w=$(echo "$QUERY_STRING" | tr '&' '\n' | sed -n "s/^w=//p")
+n=$(echo "$QUERY_STRING" | tr '&' '\n' | sed -n "s/^n=//p")
 
 yes "${w:-yes}" | head -n "${n:-1000}"
